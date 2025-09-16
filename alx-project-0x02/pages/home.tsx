0af6ffb9be9b36b7523fa1b47cd4e0256ab67ff8
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-16 11:23:42 +0000

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Card from "../components/common/Card";
 
 export default function HomePage() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the Home page of the app.</p>
+      <Card
+        title="First Card"
+        content="This is the content of the first reusable card."
+      />
+      <Card
+        title="Second Card"
+        content="Here’s another card with different content."
+      />
+      <Card
+        title="Third Card"
+        content="You can add as many cards as you like, just pass different props."
+      />
     </div>
   );
 }
