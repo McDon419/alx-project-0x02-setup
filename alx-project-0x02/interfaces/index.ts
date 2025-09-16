a0298e4957d4f8546e6384b52b6de2c103d2ff83
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-16 11:40:25 +0000

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -9,3 +9,11 @@ export interface CardProps {
   content: string;
 }
 
+export interface ButtonProps {
+  label: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+}
+
+
