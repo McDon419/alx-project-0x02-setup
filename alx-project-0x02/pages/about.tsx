f1fc91421cebe3e53309e4f96bbc639d7d67e698
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-16 11:41:48 +0000

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Button from "../components/common/Button";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the About page of the app.</p>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">About Us</h1>
+
+      <div className="flex gap-4">
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
     </div>
   );
 }
